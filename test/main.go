279d0602d61ece3207152021e73a7dc75b51f9b4
wangyuan249 <249401644@qq.com>
2021-08-10 17:34:15 +0800

    update api

--- test/main.go
+++ test/main.go
@@ -4,16 +4,16 @@ import (
 	"context"
 	"log"
 
-	core_oam_dev "github.com/oam-dev/kubevela-core-api/apis/core.oam.dev"
-	core_v1alpha2 "github.com/oam-dev/kubevela-core-api/apis/core.oam.dev/v1alpha2"
-	kubevelaapistandard "github.com/oam-dev/kubevela-core-api/apis/standard.oam.dev/v1alpha1"
-
-	appsv1 "k8s.io/api/apps/v1"
-	v1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
+
+	core_oam_dev "github.com/oam-dev/kubevela-core-api/apis/core.oam.dev"
+	"github.com/oam-dev/kubevela-core-api/apis/core.oam.dev/common"
+	core_v1beta1 "github.com/oam-dev/kubevela-core-api/apis/core.oam.dev/v1beta1"
+	kubevelaapistandard "github.com/oam-dev/kubevela-core-api/apis/standard.oam.dev/v1alpha1"
+	"github.com/oam-dev/kubevela-core-api/pkg/oam/util"
 )
 
 var scheme = runtime.NewScheme()
@@ -28,38 +28,103 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	err = k8sClient.Create(context.Background(), &core_v1alpha2.Component{
-		ObjectMeta: metav1.ObjectMeta{Name: "test", Namespace: "default"},
-		Spec: core_v1alpha2.ComponentSpec{Workload: runtime.RawExtension{
-			Object: &appsv1.Deployment{
-				TypeMeta: metav1.TypeMeta{
+	err = k8sClient.Create(context.Background(), &core_v1beta1.ComponentDefinition{
+		TypeMeta: metav1.TypeMeta{
+			Kind:       "ComponentDefinition",
+			APIVersion: "core.oam.dev/v1beta1",
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "test-comp",
+			Namespace: "vela-system",
+		},
+		Spec: core_v1beta1.ComponentDefinitionSpec{
+			Workload: common.WorkloadTypeDescriptor{
+				Definition: common.WorkloadGVK{
 					Kind:       "Deployment",
 					APIVersion: "apps/v1",
 				},
-				Spec: appsv1.DeploymentSpec{
-					Selector: &metav1.LabelSelector{
-						MatchLabels: map[string]string{"app": "test"},
-					},
-					Template: v1.PodTemplateSpec{
-						ObjectMeta: metav1.ObjectMeta{
-							Labels: map[string]string{"app": "test"},
-						},
-						Spec: v1.PodSpec{
-							Containers: []v1.Container{
-								{Name: "image1", Image: "nginx"},
-							},
-						},
-					},
-				}},
+			},
+			Schematic: &common.Schematic{
+				CUE: &common.CUE{
+					Template: webServiceTemplate,
+				},
+			},
 		},
-		}})
+	})
+
 	if err != nil {
 		log.Fatal(err)
 	}
-	err = k8sClient.Create(context.Background(), &core_v1alpha2.ApplicationConfiguration{
-		ObjectMeta: metav1.ObjectMeta{Name: "test-app", Namespace: "default"},
-		Spec:       core_v1alpha2.ApplicationConfigurationSpec{Components: []core_v1alpha2.ApplicationConfigurationComponent{{ComponentName: "test"}}}})
+
+	err = k8sClient.Create(context.Background(), &core_v1beta1.Application{
+		TypeMeta: metav1.TypeMeta{
+			Kind:       "Application",
+			APIVersion: "core.oam.dev/v1beta1",
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "test-app",
+			Namespace: "default",
+		},
+		Spec: core_v1beta1.ApplicationSpec{
+			Components: []common.ApplicationComponent{
+				{
+					Name: "web",
+					Type: "webservice",
+					Properties: util.Object2RawExtension(map[string]interface{}{
+						"image": "nginx:1.14.0",
+					}),
+					Traits: []common.ApplicationTrait{
+						{
+							Type: "labels",
+							Properties: util.Object2RawExtension(map[string]interface{}{
+								"hello": "world",
+							}),
+						},
+					},
+				},
+			},
+		},
+	})
 	if err != nil {
 		log.Fatal(err)
 	}
 }
+
+var webServiceTemplate = `output: {
+	apiVersion: "apps/v1"
+	kind:       "Deployment"
+	metadata: labels: {
+		"componentdefinition.oam.dev/version": "v1"
+	}
+	spec: {
+		selector: matchLabels: {
+			"app.oam.dev/component": context.name
+		}
+		template: {
+			metadata: labels: {
+				"app.oam.dev/component": context.name
+			}
+			spec: {
+				containers: [{
+					name:  context.name
+					image: parameter.image
+					if parameter["cmd"] != _|_ {
+						command: parameter.cmd
+					}
+					if context["config"] != _|_ {
+						env: context.config
+					}
+					ports: [{
+						containerPort: parameter.port
+					}]
+				}]
+		    }
+		}
+	}
+}
+parameter: {
+	image: string
+	cmd?: [...string]
+	port: *80 | int
+}
+`
